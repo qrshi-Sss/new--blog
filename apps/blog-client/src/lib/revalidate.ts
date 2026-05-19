b2307030d6cd83ac532bd0e0ebf57d00e026dd3a
shiqiaorui <2467383712@qq.com>
2026-05-19 20:55:30 +0800

    feat: 线上部署

--- apps/blog-client/src/lib/revalidate.ts
+++ apps/blog-client/src/lib/revalidate.ts
@@ -1,5 +1,5 @@
-const REVALIDATE_SECRET = process.env.REVALIDATE_SECRET || ''
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+const REVALIDATE_SECRET = process.env.REVALIDATE_SECRET
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
 
 interface RevalidateOptions {
   type: 'category' | 'doc' | 'home'
--- apps/blog-service/src/module/content/content.module.ts
+++ apps/blog-service/src/module/content/content.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ContentController } from './content.controller'
 import { ContentService } from './content.service'
+import { WebhookService } from '../webhook/webhook.service'
 import { CategoryEntity } from './entities/category.entity'
 import { DocEntity } from './entities/doc.entity'
 
 @Module({
   imports: [TypeOrmModule.forFeature([CategoryEntity, DocEntity])],
   controllers: [ContentController],
-  providers: [ContentService],
+  providers: [ContentService, WebhookService],
   exports: [ContentService]
 })
 export class ContentModule {}

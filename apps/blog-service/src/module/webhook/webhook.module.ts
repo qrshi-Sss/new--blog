564f586d3193fbf3b3fa653835c593e6cb923645
sqr <2467383712@qq.com>
2026-07-06 20:40:26 +0800

    refactor: 删除不再使用的重验证相关代码和配置

--- apps/blog-service/src/module/webhook/webhook.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common'
-import { WebhookService } from './webhook.service'
-
-@Global()
-@Module({
-  providers: [WebhookService],
-  exports: [WebhookService]
-})
-export class WebhookModule {}
